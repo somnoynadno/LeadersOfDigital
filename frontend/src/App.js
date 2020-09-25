a6696e302a532f4d82ba2c22eb1b839292cda10e
Alexander Zorkin <somnoynadno@yandex.ru>
2020-09-25 22:38:16 +0700

    Pages structure in React Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,38 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
+import {Route, Switch, withRouter} from "react-router-dom"
 
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    const { history } = this.props;
+
+    return (
+        <div className="App">
+          <Switch>
+            {/* Client */}
+            <Route history={history} path='/applications' component={null} />
+            <Route history={history} path='/new_application' component={null} />
+            {/* Employee */}
+            <Route history={history} path='/free_applications' component={null} />
+            <Route history={history} path='/my_applications' component={null} />
+            {/* Authorization */}
+            <Route history={history} path='/auth' component={null} />
+            <Route history={history} path='/login' component={null} />
+            <Route history={history} path='/register' component={null} />
+            {/* Common */}
+            <Route history={history} path='/application/:application_id' component={null} />
+            <Route exact history={history} path='/' component={null}/>
+            <Route exact history={history} path='/logout'><Logout /></Route>
+          </Switch>
+        </div>
+    );
+  }
 }
 
-export default App;
+function Logout() {
+  localStorage.removeItem('token');
+  localStorage.removeItem('IsEmployee');
+  window.location.href='/login';
+}
+
+export default withRouter(App);
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,10 +4,17 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import {Router} from "react-router-dom"
+import {createBrowserHistory} from 'history';
+
+const history = createBrowserHistory()
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <Router history={history}>
+            <App/>
+        </Router>
+    </React.StrictMode>,
   document.getElementById('root')
 );
 
