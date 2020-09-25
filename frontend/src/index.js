d7c4dad58d06b4401b85842b3a0d911a63cdcebb
Alexander Zorkin <somnoynadno@yandex.ru>
2020-09-26 02:07:41 +0700

    Login page style changed, history bug fixed

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-import {Router} from "react-router-dom"
-import {createBrowserHistory} from 'history';
+import {Router} from "react-router-dom";
+import { createBrowserHistory } from 'history'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
--- frontend/src/pages/auth/Login.js
+++ frontend/src/pages/auth/Login.js
@@ -10,54 +10,61 @@ import '../../styles/Login.css';
 class Login extends React.Component {
     constructor(props) {
         super(props);
-
-        this.state = {title: "Вход"}
     }
 
-    render () {
-        return <div id={"main"}>
-                <div className={"wrapper"}>
-                    <div className={"content"}>
+    render() {
+        return (
+            <Container className="h-100" style={containerStyle}>
+                {/* TODO: add style */}
+                <Row className="justify-content-center align-items-center p-3" style={formStyle}>
+                    <Col className="col-auto text-center p-4 align-self-center">
                         <h5>Я клиент Газпромбанка</h5>
                         <Form id={"client-form"}>
                             <Form.Group controlId="formBasicEmail">
-                                <Form.Control type="email" placeholder="E-Mail" />
+                                <Form.Control type="email" placeholder="E-Mail"/>
                             </Form.Group>
 
                             <Form.Group controlId="formBasicPassword">
-                                <Form.Control type="password" placeholder="Пароль" />
+                                <Form.Control type="password" placeholder="Пароль"/>
                             </Form.Group>
 
-                            <Button variant="primary" type="submit">
+                            <Button variant="primary" type="submit" className={"mb-2"}>
                                 Войти
                             </Button>
-                            <div id={"registration-button"}>
-                                <a href={"idite_nahuy"}>Зарегистрироваться</a>
+                            <div id={"registration-button"} className={"link-style-2"}>
+                                <span>Зарегистрироваться</span>
                             </div>
                         </Form>
+                    </Col>
 
-                    </div>
-                </div>
-                <div className={"wrapper"}>
-                    <div className={"content"}>
+                    <Col className="col-auto text-center p-4 align-self-center mb-4">
                         <h5>Я сотрудник Газпромбанка</h5>
                         <Form id={"employee-form"}>
                             <Form.Group controlId="formBasicEmail">
-                                <Form.Control type="email" placeholder="E-Mail" />
+                                <Form.Control type="email" placeholder="E-Mail"/>
                             </Form.Group>
 
                             <Form.Group controlId="formBasicPassword">
-                                <Form.Control type="password" placeholder="Пароль" />
+                                <Form.Control type="password" placeholder="Пароль"/>
                             </Form.Group>
 
                             <Button variant="primary" type="submit">
                                 Войти
                             </Button>
                         </Form>
-                    </div>
-                </div>
-            </div>
+                    </Col>
+                </Row>
+            </Container>
+        )
     }
 }
 
 export default Login;
+
+let formStyle = {
+    height: "100vh !important"
+}
+
+let containerStyle = {
+    margin: "8% auto"
+}
